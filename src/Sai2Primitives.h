ccd4387be27ed8d1ce50661daf3e6adf7ac4b25e
Mikael <mjorda@stanford.edu>
2019-09-25 18:07:27 -0700

    removed unfinished stuff and stuff not relevant to JD for clarity purposes

--- src/Sai2Primitives.h
+++ src/Sai2Primitives.h
@@ -2,15 +2,7 @@
 #include "tasks/PositionTask.h"
 #include "tasks/OrientationTask.h"
 #include "tasks/PosOriTask.h"
-#include "tasks/SupportAndConstraintsTask.h"
 #include "tasks/TwoHandTwoRobotsTask.h"
-#include "primitives/RedundantArmMotion.h"
-#include "primitives/SurfaceSurfaceAlignment.h"
-#include "primitives/DualArmObjectMotion.h"
-
-#include "haptic_tasks/OpenLoopTeleop.h"
-#include "haptic_tasks/HapticController.h"
-#include "haptic_tasks/BilateralPassivityController.h"
 
 #ifdef USING_OTG
 #include "trajectory_generation/OTG.h"
